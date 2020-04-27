52c4cbf4c9d931f3fddde04999ec1806001a3257
marcelo <marces2000@gmail.com>
2020-04-26 23:47:19 -0300

    x

--- bbbb/src/main/java/com/bienes/view/pdf/ExpedienteMovimientoPdfView.java
+++ bbbb/src/main/java/com/bienes/view/pdf/ExpedienteMovimientoPdfView.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Component;
  
 import org.springframework.web.servlet.view.document.AbstractPdfView;
