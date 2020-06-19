ec82e7eeeae50c1a082c5cad36f9d9a37b22a3dc
marcelo <marcelo.schell@intraway.com>
2020-06-19 14:57:07 -0300

    n

---
+++ bbbb/src/main/java/com/bienes/model/Rubro.java
@@ -0,0 +1,25 @@
+package com.bienes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "rubros")
+public class Rubro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "rubros_seq_gen")
+	@SequenceGenerator(name = "rubros_seq_gen", sequenceName = "rubros_id_seq",allocationSize=1)
+	private Integer id;
+	
+	@NotNull
+	private String nombre;
+	
+	private static final long serialVersionUID = 1L;
+
+}
